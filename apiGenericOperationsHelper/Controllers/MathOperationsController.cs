e657f4d3cce934d0b244b3702850be2d66619be4
Tushar <tushar.pawar@gmail.com>
2020-06-14 00:06:04 -0400

    Add project files.

---
+++ apiGenericOperationsHelper/Controllers/MathOperationsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiGenericOperationsHelper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathOperationsController : ControllerBase
+    {
+        [HttpGet("AddNumbers")]
+        [Route("api/[controller]/AddNumbers")]
+        public int AddNumbers(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
